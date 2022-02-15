e447353823c1ae18cee77d6ff379c8d974f9f570
sachinmewar <sachinmewar800@gmail.com>
2022-02-15 11:40:21 +0530

    Spring Boot Applicatin

---
+++ src/main/java/com/example/demoprojectforspringsecurity/DemoProjectForSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoprojectforspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectForSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectForSpringSecurityApplication.class, args);
+	}
+
+}
