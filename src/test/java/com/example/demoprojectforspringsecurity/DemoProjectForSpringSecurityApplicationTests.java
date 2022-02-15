0178542fe966225c1edb7571422ad9c6dc5997dc
sachinmewar <sachinmewar800@gmail.com>
2022-02-15 11:44:22 +0530

    src

---
+++ src/test/java/com/example/demoprojectforspringsecurity/DemoProjectForSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoprojectforspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectForSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
